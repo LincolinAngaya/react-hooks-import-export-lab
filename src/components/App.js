a260c0086ceea4b10a39496a2c700bb1e507d661
LincolinAngaya <lincolinangaya5626@gmail.com>
2022-07-13 11:14:47 +0300

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
 import React from "react";
-
+import Home from './Home';
+import NavBar from "./NavBar";
+import About from "./About";
+import {username,city} from './Home';
+import {image} from './About';
 function App() {
+
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home  username="Liza" city="New York"/>
+      <About image="https://i.imgur.com/mV8PQxj.gif" />
     </div>
   );
 }
